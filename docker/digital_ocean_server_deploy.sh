5f10ca387e5684e800b98f723a1b11fa3e2afa5e
keshavcode16 <webevnt2@gmail.com>
2024-05-12 22:19:51 +0530

    deploy script

--- docker/digital_ocean_server_deploy.sh
+++ docker/digital_ocean_server_deploy.sh
@@ -6,14 +6,14 @@ then
   exit 0
 fi
 
-git archive --format tar --output ./project.tar main
+git archive --format tar --output ./project.tar master
 
 echo 'Uploading project.......:-)...Be Patient!'
-rsync ./project.tar root@DIGITAL_OCEAN_IP_ADDRESS:/tmp/project.tar
+rsync ./project.tar root@$DIGITAL_OCEAN_IP_ADDRESS:/tmp/project.tar
 echo 'Upload complete...:-)'
 
 echo 'Building the image...'
-ssh -o StrictHostKeyChecking=no root@DIGITAL_OCEAN_IP_ADDRESS << 'ENDSSH'
+ssh -o StrictHostKeyChecking=no root@$DIGITAL_OCEAN_IP_ADDRESS << 'ENDSSH'
   mkdir -p /app
   rm -rf /app/* && tar -xf /tmp/project.tar -C /app
   docker compose -f /app/production.yml up --build -d --remove-orphans
